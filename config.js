1be6ffb45190b11de8e199ad42cc85ecf0bdc910
PimTournaye <pimtournaye@student.ehb.be>
2022-04-20 13:12:34 +0200

    spacing

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-
 // booleans for startup / init
 export let interactiveMode, active, maxMSP, modeMIDI, keyChangeEvent;
 active = true   // actives the main loop if true, mostly is here to see if everything sets up correctly
 interactiveMode = true;   // if true, the balls will be created by the user with keyboard input, check which buttons in resonance.js near the bottom of the file
-maxMSP = false; // if true, the program will function as if it's a Max MSP project
+maxMSP = false; // if true, the program will function as if it's a Node for Max project
 modeMIDI = true // if true, the program will function as if talking to a MIDI device
-keyChangeEvent = true // if true, this will trigger a key change if there are no balls for a certain amount of time (configured in KEY_CHANGE_TIMER below)
+keyChangeEvent = true // if true, this will trigger a key change if there are no balls for a certain amount of time (configured in KEY_CHANGE_TIMER below) [EXPERIMENTAL]
 
-// MIDI INPUT / OUTPUT DEVICE -- CHANGE TO THE DEVICE YOU WANT TO USE
+// MIDI INPUT / OUTPUT DEVICE -- CHANGE TO THE DEVICE YOU WANT TO USE IF USING MIDI
 export const MIDI_DEVICE_NAME = "IAC Driver Bus 1";
 // Time needed to change the scale after inactivity
 export const KEY_CHANGE_TIMER = 10000;
